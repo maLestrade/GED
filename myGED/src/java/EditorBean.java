0f2d49d1dfad4a320e0db56a883a7eaee575405a
Cogotch <Cogotch@cogotch>
2013-10-09 16:31:36 +0200

    ajout editeur

---
+++ myGED/src/java/EditorBean.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Cogotch
+ */
+public class EditorBean {
+    private String value;  
+  
+    public String getValue() {  
+        return value;  
+    }  
+  
+    public void setValue(String value) {  
+        this.value = value;  
+    } 
+}
